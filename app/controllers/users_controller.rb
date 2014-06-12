d263680908700c8a5966768f826cb8f10175ef87
vivit <kazyu.vanilla@gmail.com>
2014-06-13 05:57:49 +0900

    bootstrap導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.order("id ASC")
   end
 
   # GET /users/1
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
+# encoding: utf-8
+
 module UsersHelper
+  def att_j(att)
+    return "在室" if(att)
+    ""
+  end
 end
